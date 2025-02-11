1699ad5244ada1bc3e3014283c4b61e94cde7b4c
Anurup Chandra Mondal <anurup.mondal75@gmail.com>
2025-02-11 20:06:20 +0530

    made order item

---
+++ src/main/java/com/restaurant/OrderItem.java
@@ -0,0 +1,25 @@
+package com.restaurant;
+
+public class OrderItem {
+    private String itemName;
+    private int quantity;
+    private double price;
+
+    public OrderItem(String itemName, int quantity, double price) {
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
